2daddcb14e4f1f2d1a02cb670bdad528cad6276f
welborau <welborau@onid.oregonstate.edu>
2015-02-02 20:39:05 -0700

    addition of form on multtable.php

--- src/multtable.php
+++ src/multtable.php
@@ -1,26 +1,45 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Assigment 4 Multtable</title>
+	<title>Multtable</title>
+	<h1> Assignment 4 Multtable.php </h1>
 </head>
 <body>
+	<legend><strong>Multtable Generator<strong></legend>
 	<fieldset>
 		<form action="multtable.php" method="get">
-		<label>Min Mulitplicand:
-			<input type="text" name="min-multiplicand"/></label>
+		<label>Min Mulitplicand: 
+			<input type="text" name="min-multiplicand" size="10" /></label><br/>
 
 		<label>Max Multiplicand:
-			<input type="text" name="max-multiplicand"/></label>
+			<input type="text" name="max-multiplicand" size="10" /></label><br/>
 
-		<label>Min Multiplier:
-			<input type="text" name="min-multiplier"/></label>
+		<label>Min Multiplier:   
+			<input type="text" name="min-multiplier" size="10" /></label><br/>
 
-		<label>Max Multiplier:
-			<input type="text" name="max-multiplier"/></label><br/>
-			<input type="submit" name="calculate" value="Calculate"/>
+		<label>Max Multiplier:  
+			<input type="text" name="max-multiplier" size="10" /></label><br/>
+			<input type="submit" name="calculate" value="Create Table"/>
 		</form>
 	</fieldset>	
 		<div id="multtable">
 		</div>
 </body>	
+</html>
+
+
+<html>
+<body>
+<div>
+<?php
+	if ($_GET["min-multiplicand"] > $_GET["max-multiplicand"]) {
+		echo "The min multiplicand should be less than the max multiplicand!";
+	}
+
+	if ($_GET["min-multiplier"] > $_GET["max-multiplier"]) {
+		echo "The min multiplier should be less than the max multiplier!";
+	}
+?>
+</div>
+</body>
 </html>
