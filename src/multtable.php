ced362a6ff3edd6b37e1d13138d92bdbe7189c5b
welborau <welborau@onid.oregonstate.edu>
2015-02-04 02:39:20 -0700

    update to multtable.php and log.txt

--- src/multtable.php
+++ src/multtable.php
@@ -1,74 +1,78 @@
 <!DOCTYPE html>
 
-<?php
-	while ($_GET["min-multiplicand"] > $_GET["max-multiplicand"]) {
-		trigger_error("The min-multiplicand can't be greater than the max-multiplicand!");
-		break;
-	}
-
-	while ($_GET["min-multiplier"] > $_GET["max-multiplier"]) {
-		echo "The min multiplier should be less than the max multiplier!";
-		break;
-	}
-?>
-
-<?php
-	$n_cols = _GET["max-multiplier"]-_GET["min-multiplier"] + 2;
-	$n_rows = _GET["max-multiplicand"] - _GET["min-multiplicand"] + 2;
-	$f_cols_val = $cols_val = _GET["min-multiplier"];
-	$f_rows_val = $rows_val = _GET["min-multiplicand"];
-	
-	$table = new HTML_Table();
-	// set the 0,0 cell to empty
-	$table->setCellContents(0, 0,'');
-
-	// set the first column values
-	for($i = 1; $i < $n_rows; i++) 
-	{
-		setCellContents(i, 0, $f_rows_val);
-		$f_rows_val++;
-	}
-	// set the first row values
-	for($i = 1; $i < $n_cols; i++)
-	{
-		$table->setCellContents(0, i, $f_cols_val);
-		$f_cols_val++;
-	}
-	// loop through each rows/columns and assign each value
-	for ($row = 1; $row < $n_rows; $rows++) {
-		for ($cols = 1; $cols < $n_cols; $cols++)
-		{
-			$table->setCellContents($row, $cols, $cols_val*$rows_val);
-			$cols_val++;
-		}
-		$rows_val++;
-	} 	
-	echo $table->toHTML();
-?>
 
 <html>
 <head>
 	<title>Multtable</title>
 	<h1> Assignment 4 Multtable.php </h1>
 </head>
-<body>
-	<legend><strong>Multtable Generator<strong></legend>
-	<fieldset>
-		<form action="http://web.engr.oregonstate.edu/~welborau/CS290/multtable.php" method="get">
-		<label>Min Multiplicand: 
-			<input type="text" name="min-multiplicand" size="10" /></label><br/>
+<body>	
+	<?php
 
-		<label>Max Multiplicand:
-			<input type="text" name="max-multiplicand" size="10" /></label><br/>
+	while ($_GET["min-multiplicand"] > $_GET["max-multiplicand"]) {
+		echo "The min-multiplicand can't be greater than the max-multiplicand!";
+		exit(1);
+	}
 
-		<label>Min Multiplier:   
-			<input type="text" name="min-multiplier" size="10" /></label><br/>
+	while ($_GET["min-multiplier"] > $_GET["max-multiplier"]) {
+		echo "The min multiplier should be less than the max multiplier!";
+		exit(1);
+	}
 
-		<label>Max Multiplier:  
-			<input type="text" name="max-multiplier" size="10" /></label><br/>
-			<input type="submit" name="calculate" value="Create Table"/>
-		</form>
-	<fieldset>	
+	while (is_null($_GET["min-multiplier"] || !is_int($_GET["min-multiplier"])) 
+		echo "Missing/incorrect input for min-multiplier!";
+		exit(1);
+	}
+	while (is_null($_GET["max-multiplier"] || !is_int($_GET["max-multiplier"])) 
+		echo "Missing/incorrect input for max-multiplier!";
+		exit(1);
+	}
+	while (is_null($_GET["min-multiplicand"] || !is_int($_GET["min-multiplicand"])) 
+		echo "Missing/incorrect input for min-multiplicand!";
+		exit(1);
+	}
+	while (is_null($_GET["max-multiplicand"] || !is_int($_GET["max-multiplicand"])) 
+		echo "Missing/incorrect input for max-multiplicand!";
+		exit(1);
+	}
+
+	$n_cols = $_GET["max-multiplier"] - $_GET["min-multiplier"] + 2;
+	$n_rows = $_GET["max-multiplicand"] - $_GET["min-multiplicand"] + 2;
+	$f_cols_val = $cols_val = $_GET["min-multiplier"];
+	$f_rows_val = $rows_val = $_GET["min-multiplicand"];
+	
+	?><table>
+	<tr> 
+		<th></th>
+		<?php
+		// set the first row values
+		for($i = 1; $i < $n_cols; i++)
+		{
+			?><th><?php $f_cols_val ?></th>
+			<?php
+			$f_cols_val++;
+		}
+	?></tr>
+		<?php
+		// set the first column values
+		for($i = 1; $i < $n_rows; i++) 
+		{
+			?><th><?php $f_rows_val ?><th>
+			<?php
+			$f_rows_val++;
+		}
+
+		// loop through each rows/columns and assign each value
+		for ($row = 1; $row < $n_rows; $rows++) {
+			for ($cols = 1; $cols < $n_cols; $cols++)
+			{
+				$table->setCellContents($row, $cols, $cols_val*$rows_val);
+				$cols_val++;
+			}
+			$rows_val++;
+		} 	
+?>
+<table>
 </body>	
 </html>
 
