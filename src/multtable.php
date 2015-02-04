3e88882bbfd7dd8b95e679c8a0432990cd653343
welborau <welborau@onid.oregonstate.edu>
2015-02-03 22:51:32 -0700

    added table using php
    added a table using php to multtable but thinking this isn't correct, it
    seems to require the usage of pear

--- src/multtable.php
+++ src/multtable.php
@@ -1,4 +1,51 @@
 <!DOCTYPE html>
+
+<?php
+	while ($_GET["min-multiplicand"] > $_GET["max-multiplicand"]) {
+		trigger_error("The min-multiplicand can't be greater than the max-multiplicand!");
+		break;
+	}
+
+	while ($_GET["min-multiplier"] > $_GET["max-multiplier"]) {
+		echo "The min multiplier should be less than the max multiplier!";
+		break;
+	}
+?>
+
+<?php
+	$n_cols = _GET["max-multiplier"]-_GET["min-multiplier"] + 2;
+	$n_rows = _GET["max-multiplicand"] - _GET["min-multiplicand"] + 2;
+	$f_cols_val = $cols_val = _GET["min-multiplier"];
+	$f_rows_val = $rows_val = _GET["min-multiplicand"];
+	
+	$table = new HTML_Table();
+	// set the 0,0 cell to empty
+	$table->setCellContents(0, 0,'');
+
+	// set the first column values
+	for($i = 1; $i < $n_rows; i++) 
+	{
+		setCellContents(i, 0, $f_rows_val);
+		$f_rows_val++;
+	}
+	// set the first row values
+	for($i = 1; $i < $n_cols; i++)
+	{
+		$table->setCellContents(0, i, $f_cols_val);
+		$f_cols_val++;
+	}
+	// loop through each rows/columns and assign each value
+	for ($row = 1; $row < $n_rows; $rows++) {
+		for ($cols = 1; $cols < $n_cols; $cols++)
+		{
+			$table->setCellContents($row, $cols, $cols_val*$rows_val);
+			$cols_val++;
+		}
+		$rows_val++;
+	} 	
+	echo $table->toHTML();
+?>
+
 <html>
 <head>
 	<title>Multtable</title>
@@ -7,8 +54,8 @@
 <body>
 	<legend><strong>Multtable Generator<strong></legend>
 	<fieldset>
-		<form action="multtable.php" method="get">
-		<label>Min Mulitplicand: 
+		<form action="http://web.engr.oregonstate.edu/~welborau/CS290/multtable.php" method="get">
+		<label>Min Multiplicand: 
 			<input type="text" name="min-multiplicand" size="10" /></label><br/>
 
 		<label>Max Multiplicand:
@@ -21,25 +68,7 @@
 			<input type="text" name="max-multiplier" size="10" /></label><br/>
 			<input type="submit" name="calculate" value="Create Table"/>
 		</form>
-	</fieldset>	
-		<div id="multtable">
-		</div>
+	<fieldset>	
 </body>	
 </html>
 
-
-<html>
-<body>
-<div>
-<?php
-	if ($_GET["min-multiplicand"] > $_GET["max-multiplicand"]) {
-		echo "The min multiplicand should be less than the max multiplicand!";
-	}
-
-	if ($_GET["min-multiplier"] > $_GET["max-multiplier"]) {
-		echo "The min multiplier should be less than the max multiplier!";
-	}
-?>
-</div>
-</body>
-</html>
